6652dff8a27873bc904ecea82a49e4233599e115
Haram Kwon <kharam@outlook.com>
2019-03-17 16:34:46 -0700

    motif size from 5 to 4

--- run_test.sh
+++ run_test.sh
@@ -17,11 +17,11 @@ echo "test 01: mytest.txt motif:3 random:1000 undirected"
 echo "" >> result.txt
 
 ## test 01-2
-echo "test 02: mytest.txt motif:5 random:1000 undirected" >> result.txt
-echo "test 02: mytest.txt motif:5 random:1000 undirected" 
-./nemolib test_samples/mytest.txt 5 1000 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/mytest.txt 5 1000 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/mytest.txt 5 1000 0 | grep -i "Time" >> result.txt
+echo "test 02: mytest.txt motif:4 random:1000 undirected" >> result.txt
+echo "test 02: mytest.txt motif:4 random:1000 undirected" 
+./nemolib test_samples/mytest.txt 4 1000 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/mytest.txt 4 1000 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/mytest.txt 4 1000 0 | grep -i "Time" >> result.txt
 echo "" >> result.txt
 
 ## test 01-3
@@ -33,11 +33,11 @@ echo "test 03: mytest.txt motif:3 random:1500 undirected"
 echo "" >> result.txt
 
 ## test 01-4
-echo "test 04: mytest.txt motif:5 random:1500 undirected" >> result.txt
-echo "test 04: mytest.txt motif:5 random:1500 undirected" 
-./nemolib test_samples/mytest.txt 5 1500 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/mytest.txt 5 1500 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/mytest.txt 5 1500 0 | grep -i "Time" >> result.txt
+echo "test 04: mytest.txt motif:4 random:1500 undirected" >> result.txt
+echo "test 04: mytest.txt motif:4 random:1500 undirected" 
+./nemolib test_samples/mytest.txt 4 1500 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/mytest.txt 4 1500 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/mytest.txt 4 1500 0 | grep -i "Time" >> result.txt
 echo "" >> result.txt
 echo "------------------------------------------------------------------" >> result.txt
 echo "\n" >> result.txt
@@ -52,11 +52,11 @@ echo "test 01: MIPS_index.txt motif:3 random:1000 undirected"
 echo "" >> result.txt
 
 ## test 02-2
-echo "test 02: MIPS_index.txt motif:5 random:1000 undirected" >> result.txt
-echo "test 02: MIPS_index.txt motif:5 random:1000 undirected" 
-./nemolib test_samples/MIPS_index.txt 5 1000 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/MIPS_index.txt 5 1000 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/MIPS_index.txt 5 1000 0 | grep -i "Time" >> result.txt
+echo "test 02: MIPS_index.txt motif:4 random:1000 undirected" >> result.txt
+echo "test 02: MIPS_index.txt motif:4 random:1000 undirected" 
+./nemolib test_samples/MIPS_index.txt 4 1000 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/MIPS_index.txt 4 1000 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/MIPS_index.txt 4 1000 0 | grep -i "Time" >> result.txt
 echo "" >> result.txt
 
 ## test 02-3
@@ -68,11 +68,11 @@ echo "test 03: MIPS_index.txt motif:3 random:1500 undirected"
 echo "" >> result.txt
 
 ## test 02-4
-echo "test 04: MIPS_index.txt motif:5 random:1500 undirected" >> result.txt
-echo "test 04: MIPS_index.txt motif:5 random:1500 undirected" 
-./nemolib test_samples/MIPS_index.txt 5 1500 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/MIPS_index.txt 5 1500 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/MIPS_index.txt 5 1500 0 | grep -i "Time" >> result.txt
+echo "test 04: MIPS_index.txt motif:4 random:1500 undirected" >> result.txt
+echo "test 04: MIPS_index.txt motif:4 random:1500 undirected" 
+./nemolib test_samples/MIPS_index.txt 4 1500 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/MIPS_index.txt 4 1500 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/MIPS_index.txt 4 1500 0 | grep -i "Time" >> result.txt
 echo "" >> result.txt
 echo "------------------------------------------------------------------" >> result.txt
 echo "\n" >> result.txt
@@ -87,11 +87,11 @@ echo "test 01: Dmela20160430_idx.txt motif:3 random:1000 undirected"
 echo "" >> result.txt
 
 ## test 02-2
-echo "test 02: Dmela20160430_idx.txt motif:5 random:1000 undirected" >> result.txt
-echo "test 02: Dmela20160430_idx.txt motif:5 random:1000 undirected" 
-./nemolib test_samples/Dmela20160430_idx.txt 5 1000 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/Dmela20160430_idx.txt 5 1000 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/Dmela20160430_idx.txt 5 1000 0 | grep -i "Time" >> result.txt
+echo "test 02: Dmela20160430_idx.txt motif:4 random:1000 undirected" >> result.txt
+echo "test 02: Dmela20160430_idx.txt motif:4 random:1000 undirected" 
+./nemolib test_samples/Dmela20160430_idx.txt 4 1000 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/Dmela20160430_idx.txt 4 1000 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/Dmela20160430_idx.txt 4 1000 0 | grep -i "Time" >> result.txt
 echo "" >> result.txt
 
 ## test 03-3
@@ -103,11 +103,11 @@ echo "test 03: Dmela20160430_idx.txt motif:3 random:1500 undirected"
 echo "" >> result.txt
 
 ## test 03-4
-echo "test 04: Dmela20160430_idx.txt motif:5 random:1500 undirected" >> result.txt
-echo "test 04: Dmela20160430_idx.txt motif:5 random:1500 undirected" 
-./nemolib test_samples/Dmela20160430_idx.txt 5 1500 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/Dmela20160430_idx.txt 5 1500 0 | grep -i "Time" >> result.txt
-./nemolib test_samples/Dmela20160430_idx.txt 5 1500 0 | grep -i "Time" >> result.txt
+echo "test 04: Dmela20160430_idx.txt motif:4 random:1500 undirected" >> result.txt
+echo "test 04: Dmela20160430_idx.txt motif:4 random:1500 undirected" 
+./nemolib test_samples/Dmela20160430_idx.txt 4 1500 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/Dmela20160430_idx.txt 4 1500 0 | grep -i "Time" >> result.txt
+./nemolib test_samples/Dmela20160430_idx.txt 4 1500 0 | grep -i "Time" >> result.txt
 echo "" >> result.txt
 echo "------------------------------------------------------------------" >> result.txt
 echo "\n" >> result.txt
