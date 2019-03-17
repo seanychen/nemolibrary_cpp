d21280b1f82cf95a9f75977c68d056e00fd97cf7
Haram Kwon <kharam@outlook.com>
2019-03-16 23:32:23 -0700

    - Now the constructor invoke the member initializer - int get_G_M() just implemented (merely returning G_M) - nautylabel invoke the iterator so that it is from (n*n*(2n)) -> (n*n), or simply from O(n^3) to O(n^2)

--- NautyLink.h
+++ NautyLink.h
@@ -4,78 +4,79 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   NautyLink.h
  * Author: Wooyoung
  *
  * Created on October 27, 2017, 11:36 AM
  */
-#ifndef NAUTYLINK_H
-#define NAUTYLINK_H
+#ifndef NEMOLIBRARY_C_NAUTYLINK_H
+#define NEMOLIBRARY_C_NAUTYLINK_H
+
+#define MAXN 64
 
 #include <iostream>
 #include <fstream>
-using std::cout;
-using std::cerr;
-using std::endl;
 #include <vector>
-using std::vector;
 #include <stdexcept>
-using std::domain_error;
 #include <string>
-using std::string;
-
-
-
-
-#include <unordered_map> // Like a hash_map. It should be compiled with -std=c++11 option. 
-using std::unordered_map;
-
-#define MAXN 64
-#include <string.h>
-#include <stdio.h>
-#include <math.h>
-//#include "gtools.h"
+#include <unordered_map>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
 #include "nauty/nauty.h"
 #include "graph64.hpp"
 #include "Subgraph.h"
 #include "nauty/nautinv.h"
 
+typedef uint64 graph64; //Nauty label
 
-typedef uint64 graph64;//Nauty label
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::domain_error;
+using std::string;
+using std::unordered_map;
+
 
 class NautyLink {
 public:
-    NautyLink(int, unordered_map<edge, edgetype>);
-     NautyLink(int, unordered_map<edge, edgetype>, bool);
+    NautyLink(const int &, const unordered_map<edge, edgetype> &);
+
+    NautyLink(const int &, const unordered_map<edge, edgetype> &, bool);
+
     virtual ~NautyLink();
+
     void nautyinit();
+
     int get_G_N();
+
     int get_G_M();
-    graph64 nautylabel(Subgraph&);
-    
+
+    graph64 nautylabel(Subgraph &);
+
 private:
     bool directed;
-    graph64 getLabel(graph*, set*, const int, const int);
+
+    graph64 getLabel(graph *, set *, const int &, const int &);
+
     int G_N; // subgraphsize
     int G_M;
-    graph canon[MAXN * MAXM]; 
+    graph canon[MAXN * MAXM];
     graph nautyGraph[MAXN * MAXM];
     int lab[MAXN];
     int ptn[MAXN];
     int orbits[MAXN];
-    setword workspace[160 * MAXM]; 
+    setword workspace[160 * MAXM];
     set *gv = NULL;
-    
-    //added
-    
+
+    /* ADDED
+     * TODO: GO BACK TO HERE, AND REVIEW THEM*/
     DEFAULTOPTIONS(options);
-    statsblk(stats); 
-    unordered_map<edge, edgetype> edges; // get the edges when initialize it    
-    
- 
-    
+    statsblk(stats);
+    unordered_map<edge, edgetype> edges; // get the edges when initialize it
 };
 
-#endif /* NAUTYLINK_H */
 
+#endif //NEMOLIBRARY_C_NAUTYLINK_H
